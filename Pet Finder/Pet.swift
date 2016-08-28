eaee9401801444ffb27832792183f47df4483e1d
Daniel J Janiak <danieljjaniak@icloud.com>
2016-08-28 11:39:41 -0700

    Load starter project and add a Pet data model

---
+++ Pet Finder/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  Pet Finder
+//
+//  Created by Daniel J Janiak on 8/28/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+struct Pet {
+    var name = ""
+    var type = ""
+}
